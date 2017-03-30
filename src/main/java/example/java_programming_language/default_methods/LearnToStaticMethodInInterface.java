867aa337893face257ec22de12213320d2135b52
langve <langve0+github@gmail.com>
2017-03-30 19:35:48 +0900

    #5 리펙토링

---
+++ src/main/java/example/java_programming_language/default_methods/LearnToStaticMethodInInterface.java
@@ -0,0 +1,12 @@
+package example.java_programming_language.default_methods;
+
+import static example.java_programming_language.default_methods.Person.eat;
+
+/**
+ * Created by 이영호 on 2017-03-29.
+ */
+public class LearnToStaticMethodInInterface {
+    public static void main(String[] args) {
+        eat();
+    }
+}
