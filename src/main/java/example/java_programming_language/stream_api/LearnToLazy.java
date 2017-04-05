855aa1ef89903620bc4aa62ddac5ca273086dc64
langve <langve0+github@gmail.com>
2017-04-05 15:17:41 +0900

    #1 중개연산자의 지연연산 순서 확인을 위한 추가 작업.

--- src/main/java/example/java_programming_language/stream_api/LearnToLazy.java
+++ src/main/java/example/java_programming_language/stream_api/LearnToLazy.java
@@ -7,21 +7,24 @@ import java.util.List;
  * Created by 이영호 on 2017-04-05.
  */
 public class LearnToLazy {
+    public static volatile int order = 0;
+
     public static void main(String[] args) {
         List<Person> list = new ArrayList<Person>();
 
-        list.add(new Person("m", "10"));
-        list.add(new Person("m", "12"));
-        list.add(new Person("w", "10"));
-        list.add(new Person("m", "10"));
-        list.add(new Person("m", "23"));
-        list.add(new Person("w", "10"));
-        list.add(new Person("m", "34"));
-        list.add(new Person("w", "67"));
-        list.add(new Person("w", "40"));
+        list.add(new Person("A","m", "10"));
+        list.add(new Person("B","m", "12"));
+        list.add(new Person("C","w", "10"));
+        list.add(new Person("D","m", "10"));
+        list.add(new Person("E","m", "23"));
+        list.add(new Person("F","w", "10"));
+        list.add(new Person("G","m", "34"));
+        list.add(new Person("H","w", "67"));
+        list.add(new Person("I","w", "40"));
+        list.add(new Person("J","w", "50"));
 
         list.stream().filter((p) -> p.getGender().equals("m"))
                 .filter((p) -> Integer.parseInt(p.getAge()) < 15)
-                .forEach((p) -> System.out.println("end"));
+                .forEach((p) -> System.out.println(String.format("End name : %s", p.getName())));
     }
 }
--- src/main/java/example/java_programming_language/stream_api/Person.java
+++ src/main/java/example/java_programming_language/stream_api/Person.java
@@ -4,20 +4,30 @@ package example.java_programming_language.stream_api;
  * Created by 이영호 on 2017-04-05.
  */
 public class Person {
+    private String name;
     private String gender;
     private String age;
 
-    public Person(String gender, String age){
+    public Person(String name, String gender, String age){
+        this.name = name;
         this.gender = gender;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
     public String getGender() {
-        System.out.println("getGender : " + gender);
+        System.out.println(String.format("order : %s, name : %s, getGender : %s", LearnToLazy.order, this.name, this.gender));
+        LearnToLazy.order++;
+
         return gender;
     }
 
     public String getAge() {
-        System.out.println("getAge : " + age);
+        System.out.println(String.format("order : %s, name : %s, getAge : %s", LearnToLazy.order, this.name, this.age));
+        LearnToLazy.order++;
         return age;
     }
 }
